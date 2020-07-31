55cf8464375acf8bbe873b202832565184c4d42e
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-31 12:41:35 +0530

    global styles

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
 import Home from "./screens/home";
 
 export default function App() {
-  return <Home />;
+  let [fontsLoaded] = useFonts({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Home />;
+  }
 }
