ebc39039b0d4ad9db92f0ff0bdee6534fa5bc829
vinothsmart <s.vinothkanna90@gmail.com>
2020-08-18 11:37:43 +0530

    onblur added

--- screens/reviewFrom.js
+++ screens/reviewFrom.js
@@ -33,6 +33,7 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review Title"
               onChangeText={props.handleChange("title")}
               value={props.values.title}
+              onBlur={props.handleBlur("title")}
             />
             <Text style={globalStyles.errorText}>
               {props.touched.title && props.errors.title}
@@ -43,6 +44,7 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review Body"
               onChangeText={props.handleChange("body")}
               value={props.values.body}
+              onBlur={props.handleBlur("body")}
             />
             <Text style={globalStyles.errorText}>
               {props.touched.body && props.errors.body}
@@ -53,6 +55,7 @@ export default function ReviewForm({ addReview }) {
               onChangeText={props.handleChange("rating")}
               value={props.values.rating}
               keyboardType="numeric"
+              onBlur={props.handleBlur("rating")}
             />
             <Text style={globalStyles.errorText}>
               {props.touched.rating && props.errors.rating}
